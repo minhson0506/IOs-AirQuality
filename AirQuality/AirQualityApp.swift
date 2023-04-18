ebf9540972bcedfc88536b47e64c11b5541b772f
Chi Nguyen <chi.nguyen@live.com>
2023-04-18 22:48:13 +0300

    init project

---
+++ AirQuality/AirQualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityApp.swift
+//  AirQuality
+//
+//  Created by Chi Nguyen on 18.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct AirQualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
