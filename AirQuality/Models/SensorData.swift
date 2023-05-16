f314aed6b073dfddc2380a0049a59139896eb081
Chi Nguyen <chi.nguyen@live.com>
2023-05-16 17:17:57 +0700

    add models for sensor data in date

--- AirQuality/Models/SensorData.swift
+++ AirQuality/Models/SensorData.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct SensorData: Decodable {
+    let alt: Double?
+    let co2: Double?
+    let hum: Double?
+    let lux: Double?
+    let noise: Double?
+    let pm1: Double?
+    let pm10: Double?
+    let pm2_5: Double?
+    let pm4: Double?
+    let pres: Double?
+    let temp: Double?
+    let time: String?
+}
--- AirQuality/Models/SensorDataInDate.swift
+++ AirQuality/Models/SensorDataInDate.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+typealias SensorDataInDateResult = SensorDataInDateQuery.Data
+
+struct SensorDataInDate: Decodable {
+    var sensorDataInDate: [SensorData]
+    
+    init(_ sensorDataInDate: SensorDataInDateResult?){
+        self.sensorDataInDate = sensorDataInDate?.sensorDataInDate?.compactMap {
+            guard let sensorDatum = $0 else { return nil }
+            return SensorData(alt: sensorDatum.alt, co2: sensorDatum.co2, hum: sensorDatum.hum, lux: sensorDatum.lux, noise: sensorDatum.noise, pm1: sensorDatum.pm1, pm10: sensorDatum.pm10, pm2_5: sensorDatum.pm2_5, pm4: sensorDatum.pm4, pres: sensorDatum.pres, temp: sensorDatum.temp, time: sensorDatum.time)
+        } ?? []
+    }
+}
