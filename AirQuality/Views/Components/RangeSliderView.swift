2b47f2486bc0db50412f3b8d9fd43629a89501d9
Chi Nguyen <chi.nguyen@live.com>
2023-06-02 01:33:59 +0700

    basic slider for setting

--- AirQuality/Views/Components/RangeSliderView.swift
+++ AirQuality/Views/Components/RangeSliderView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct RangeSliderView: View {
+    var name: String
+    @State private var rangeValue: RangeValue
+    
+    init(name: String, initialMinValue: Double, initialMaxValue: Double) {
+        self.name = name
+        _rangeValue = State(initialValue: RangeValue(minValue: initialMinValue, maxValue: initialMaxValue))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(name)
+            HStack {
+                Slider(value: $rangeValue.minValue, in: 0...100)
+                Slider(value: $rangeValue.maxValue, in: rangeValue.minValue...100)
+            }
+        }
+        .onChange(of: rangeValue) { newValue in
+            saveValuesToUserDefaults()
+        }
+        .onAppear {
+            loadValuesFromUserDefaults()
+        }
+    }
+    
+    private func saveValuesToUserDefaults() {
+        let defaults = UserDefaults.standard
+        defaults.set(try? PropertyListEncoder().encode(rangeValue), forKey: name)
+    }
+    
+    private func loadValuesFromUserDefaults() {
+        let defaults = UserDefaults.standard
+        if let data = defaults.value(forKey: name) as? Data,
+           let savedRangeValue = try? PropertyListDecoder().decode(RangeValue.self, from: data) {
+            rangeValue = savedRangeValue
+        }
     }
 }
 
-struct RangeSliderView_Previews: PreviewProvider {
-    static var previews: some View {
-        RangeSliderView()
-    }
+struct RangeValue: Codable, Equatable {
+    var minValue: Double
+    var maxValue: Double
 }
+
+
+
+
+
+
+
+
--- AirQuality/Views/Main/SettingsView.swift
+++ AirQuality/Views/Main/SettingsView.swift
@@ -5,21 +5,26 @@
 //  Created by Chi Nguyen on 18.4.2023.
 //
 
+
 import SwiftUI
 
 struct SettingsView: View {
+    
     var body: some View {
-        NavigationView {
-            Text("Settings")
-                .navigationTitle("Settings")
+        ScrollView {
+            VStack {
+                RangeSliderView(name: "PM10", initialMinValue: 0.0, initialMaxValue: 10)
+                RangeSliderView(name: "PM4",initialMinValue: 0.0, initialMaxValue: 10)
+                RangeSliderView(name: "PM1",initialMinValue: 0.0, initialMaxValue: 10)
+                RangeSliderView(name: "PM2.5",initialMinValue: 0.0, initialMaxValue: 10)
+                RangeSliderView(name: "Noise",initialMinValue: -100, initialMaxValue: 100)
+                RangeSliderView(name: "CO2",initialMinValue: 0.0, initialMaxValue: 1000.0)
+                RangeSliderView(name: "Pressure",initialMinValue: 0.0, initialMaxValue: 100)
+                RangeSliderView(name: "Temperature",initialMinValue: 0.0, initialMaxValue: 100)
+                RangeSliderView(name: "Light",initialMinValue: 0.0, initialMaxValue: 100)
+            }
         }
-        .navigationViewStyle(.stack)
-
+        .padding()
     }
-}
-
-struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView()
-    }
+    
 }
